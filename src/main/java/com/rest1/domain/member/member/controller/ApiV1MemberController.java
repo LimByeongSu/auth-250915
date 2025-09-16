d46d0b4270c19bc5a71bb5b583a8df0cc5ba6622
caketower <dlaqudtn1107@naver.com>
2025-09-16 14:52:25 +0900

    [14강] TDD 방식으로 ApiV1MemberController 구현. 테스트 유저 데이터는 {"newUser", "1234", "새유저"}

---
+++ src/main/java/com/rest1/domain/member/member/controller/ApiV1MemberController.java
@@ -0,0 +1,61 @@
+package com.rest1.domain.member.member.controller;
+
+
+import com.rest1.domain.member.member.dto.MemberDto;
+import com.rest1.domain.member.member.entity.Member;
+import com.rest1.domain.member.member.service.MemberService;
+import com.rest1.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class ApiV1MemberController {
+
+    private final MemberService memberService;
+
+    record JoinReqBody( //받아야하는 데이터 (요청용)
+            @NotBlank
+            @Size(min=2, max=30)
+            String username,
+
+            @NotBlank
+            @Size(min=2, max=30)
+            String password,
+
+            @NotBlank
+            @Size(min=2, max=30)
+            String nickname
+    ){
+
+    }
+
+    record JoinResBody( //응답용
+            MemberDto memberDto
+    ){
+
+    }
+
+    @PostMapping()
+    public RsData<JoinResBody> join(
+            @RequestBody @Valid JoinReqBody reqBody
+    ){
+
+        Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
+
+        return new RsData<JoinResBody>(
+                "201-1",
+                "회원가입이 완료되었습니다. %s님 환영합니다.".formatted(reqBody.nickname),
+                new JoinResBody(
+                        new MemberDto(member)
+                )
+        );
+    }
+}
