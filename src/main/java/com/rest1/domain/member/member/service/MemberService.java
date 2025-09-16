7c2db7d445d608b90fe690acb0050cb24af7aa4a
caketower <dlaqudtn1107@naver.com>
2025-09-16 10:02:41 +0900

    [3강] 글과 댓글에 작성자 등록. 작성자는 임의로 user1로 설정. self.work2() 호출 누락된 것 추가

--- src/main/java/com/rest1/domain/member/member/service/MemberService.java
+++ src/main/java/com/rest1/domain/member/member/service/MemberService.java
@@ -1,20 +1,27 @@
 package com.rest1.domain.member.member.service;
 
-import com.rest1.domain.member.member.repository.MemberRepository;
 import com.rest1.domain.member.member.entity.Member;
+import com.rest1.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     public long count(){
         return memberRepository.count();
     }
 
-    public void join(String username, String password, String nickname) {
+    public Member join(String username, String password, String nickname) {
         Member member = new Member(username, password, nickname);
+        return memberRepository.save(member);
+    }
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
     }
 }
