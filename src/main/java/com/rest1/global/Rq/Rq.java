f364d44e6cb86e383bf036cb32fcbe28867a1bd1
caketower <dlaqudtn1107@naver.com>
2025-09-17 14:55:01 +0900

    [28, 29강] 글 작성 인증 관련 예외 케이스를 테스트케이스에 추가/ 글 수정/삭제 권한 체크 테스트 케이스 추가

--- src/main/java/com/rest1/global/Rq/Rq.java
+++ src/main/java/com/rest1/global/Rq/Rq.java
@@ -4,34 +4,35 @@ import com.rest1.domain.member.member.entity.Member;
 import com.rest1.domain.member.member.service.MemberService;
 import com.rest1.global.exception.ServiceException;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.RequestScope;
 
 @Component
-@RequestScope
-@AllArgsConstructor
+//@RequestScope
+@RequiredArgsConstructor
 public class Rq {
 
     private final MemberService memberService;
-    private final HttpServletRequest request;   //요청마다 Rq가 생성되기 때문에 요청마다 api에 맞는 request값이 들어옴
-    
+    private final HttpServletRequest request;  //요청마다 Rq가 생성되기 때문에 요청마다 api에 맞는 request값이 들어옴
+
     public Member getActor() {
         // HttpServletRequest request 에서 요청에 대한 header와 응답이 나옴
 
         String authorization = request.getHeader("Authorization");
 
-        if(authorization == null && authorization.isEmpty()){
+        if(authorization == null || authorization.isEmpty()) {
             throw new ServiceException("401-1", "헤더에 인증 정보가 없습니다.");
         }
 
-        if( ! authorization.startsWith("Bearer ")){
-            throw new ServiceException("401-2", "헤더에 인증정보 형식이 올바르지 않습니다.");
+        if(!authorization.startsWith("Bearer ")) {
+            throw new ServiceException("401-2", "헤더의 인증 정보 형식이 올바르지 않습니다.");
         }
 
-        Member member = memberService.findByApiKey(authorization.replace("Bearer ", ""))
+        Member actor = memberService.findByApiKey(authorization.replace("Bearer ", ""))
                 .orElseThrow(() -> new ServiceException("401-3", "API 키가 올바르지 않습니다."));
 
-        return member;
+
+        return actor;
     }
+
 }
