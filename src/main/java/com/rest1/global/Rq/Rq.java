4f2da8926a0e9c9b94eba566a601165acd2fe7be
caketower <dlaqudtn1107@naver.com>
2025-09-17 11:46:55 +0900

    [24강] 테스트 케이스 메서드명 순서 조정, Rq 클래스 도입. Rq는 요청마다 생성되고 사라져야 한다.

---
+++ src/main/java/com/rest1/global/Rq/Rq.java
@@ -0,0 +1,10 @@
+package com.rest1.global.Rq;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class Rq {
+
+}
